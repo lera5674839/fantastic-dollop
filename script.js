76e207f45f67768ea56f5c0016b1a384b258420f
Valeria <sysoyeva-leras@mail.ru>
2024-04-01 01:02:26 +0300

    1 занятие

---
+++ script.js
@@ -0,0 +1,26 @@
+let b =3 
+console.log(b);
+
+let str1 = "Добро пожаловоть на курс Валерия"
+console.log(str1);
+
+//var c ="22.03.2024"
+//alert(c);
+
+console.log(50/0);
+
+console.log(NaN);
+
+console.log(90987656754367899567889776n);
+
+let check = true
+let offcheck = false
+console.log(10<5);
+
+console.log(null);
+
+let d 
+console.log(d);
+
+console.log(typeof false);
+document.getElementById('one').innerHTML ='Hello world'
